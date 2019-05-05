1887b4b5145ac84119dd232f9a3088ed33c6c6b6
한승우 <tuguri8@gmail.com>
2019-05-05 18:03:29 +0900

    cors 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
