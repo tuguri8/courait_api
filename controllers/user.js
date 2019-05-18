fc701740f28a884ff20bf4a02a4fac7f52ac95bf
한승우 <tuguri8@gmail.com>
2019-05-18 16:21:29 +0900

    error fix

--- controllers/user.js
+++ controllers/user.js
@@ -62,8 +62,8 @@ async function feedback(req, res) {
 async function withdrawl(req, res) {
   const { email } = req.decoded;
   const { password } = req.body;
-  const user_password = req.decoded.password;
-  console.log(`유저 패스워드${user_password}`);
+  // const user_password = req.decoded.password;
+  console.log(`유저 패스워드${req.decoded}`);
   try {
     const decipher = crypto.createDecipher('aes192', process.env.crypto_secret);
     decipher.update(user_password, 'base64', 'utf8');
