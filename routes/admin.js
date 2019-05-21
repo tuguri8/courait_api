6aa95077e5d2f8a7a242d138dbda608a519d7d7a
한승우 <tuguri8@gmail.com>
2019-05-21 18:26:49 +0900

    admin 수정

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const controller = require('../controllers');
 const authMiddleware = require('../middleware/auth');
 
+router.post('/login', controller.admin.login);
+router.post('/logout', controller.admin.logout);
 router.use(authMiddleware);
 router.get('/user/info', controller.admin.getUserInfo);
 router.get('/feedback', controller.admin.getFeedback);
 router.get('/history', controller.admin.getUserHistory);
-router.post('/login', controller.admin.login);
-router.post('/logout', controller.admin.logout);
 router.post('/new', controller.admin.newAdmin);
 router.post('/mail', controller.admin.sendMail);
 router.delete('/user/info', controller.admin.deleteUserInfo);
